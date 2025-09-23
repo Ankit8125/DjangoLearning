8502af09e70d71a5f881931762a363f4f17861f6
Ankit Verma <syncing284@gmail.com>
2025-09-23 08:45:38 +0530

    add tailwind and super user

--- Learning/chaiDjango/chaiDjango/urls.py
+++ Learning/chaiDjango/chaiDjango/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path("", views.home, name='home'), # Home page means - ''
     path("about/", views.about, name='about'),
     path("contact/", views.contact, name='contact'),
-    path('chai/', include('chai.urls')),
+    path("chai/", include('chai.urls')),
+    
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
