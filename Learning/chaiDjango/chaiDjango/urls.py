8a52a700c0b914ccbc721c988a6d2d2bce437892
Ankit Verma <syncing284@gmail.com>
2025-09-23 05:50:27 +0530

    jinja2 and django apps

--- Learning/chaiDjango/chaiDjango/urls.py
+++ Learning/chaiDjango/chaiDjango/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name='home'), # Home page means - ''
     path("about/", views.about, name='about'),
-    path("contact/", views.contact, name='contact') 
+    path("contact/", views.contact, name='contact'),
+    path('chai/', include('chai.urls')),
 ]
