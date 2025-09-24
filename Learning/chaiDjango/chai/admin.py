fb5ee7d5ea137ab199ae018648051a4955453515
Ankit Verma <syncing284@gmail.com>
2025-09-24 11:15:54 +0530

    django relationships

--- Learning/chaiDjango/chai/admin.py
+++ Learning/chaiDjango/chai/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiCertificate, ChaiReview, Store
 
 # Register your models here.
-admin.site.register(ChaiVariety)
+class ChaiReviewInline(admin.TabularInline):
+  model = ChaiReview
+  extra = 2 # we cannot give review directly, we have to give reference that for which chai we are giving review. 
+  
+class ChaiVarietyAdmin(admin.ModelAdmin):
+  # Whenever we want to customize or enable/disable features in admin, we will use ModelAdmin
+  list_display = ('name', 'type', 'date_added') # It is a tuple. Make sure all the fields mentioned here exist in model, else we will get an error.
+  inlines = [ChaiReviewInline] # We have already mentioned foreign key in models.py that is why it is able to connect
+  
+class StoreAdmin(admin.ModelAdmin):
+  list_display = ('name', 'location') # these are clickables
+  filter_horizontal = ('chai_varieties',) # If we have 1 field, then we must add a ','  
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+  list_display = ('chai', 'certificate_number')
+  
+# Now we have to inform which model we have imported and which customized class you have injected on that model
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
--- Learning/chaiDjango/chai/models.py
+++ Learning/chaiDjango/chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -17,3 +18,38 @@ class ChaiVariety(models.Model):
   
   def __str__(self):
     return self.name
+  
+# One to many
+class ChaiReview(models.Model):
+  chai = models.ForeignKey(ChaiVariety, on_delete= models.CASCADE, related_name= 'reviews') # Django automatically handles to which column we are making foreign key
+  # The keyword "CASCADE" is used to automatically update or delete related records in other tables when a record is changed in a parent table:-
+  # 1. Cascade on Delete:- When a record is deleted from a parent table, related records in child tables are also deleted.
+  # 2. Cascade on Update:- When a record is updated in a parent table, the update is also applied to related records in child tables.
+  
+  user = models.ForeignKey(User, on_delete= models.CASCADE)
+  rating = models.IntegerField()
+  comment = models.TextField()
+  date_added = models.DateTimeField(default=timezone.now)
+  
+  def __str__(self):
+    return f'{self.user.username} review for {self.chai.name}'
+  
+# Many to many
+class Store(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  chai_varieties = models.ManyToManyField(ChaiVariety, related_name= "stores") # stores is how in other tables this field will be called
+  
+  def __str__(self):
+    return self.name
+  
+# One to One
+class ChaiCertificate(models.Model):
+  chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name="certificate")
+  certificate_number = models.CharField(max_length=100)
+  issued_date = models.DateTimeField(default=timezone.now)
+  valid_until = models.DateTimeField()
+  
+  def __str__(self):
+    return f'Certificate for {self.name.chai}'
+  
